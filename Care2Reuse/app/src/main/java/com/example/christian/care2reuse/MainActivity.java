52e3945fe569c9ba0195cb5c22a8e6e08bb2e8c3
BenjaminSorterup <benjamin@sorterup.com>
2015-04-24 13:56:11 +0200

    Last working version

--- Care2Reuse/app/src/main/java/com/example/christian/care2reuse/MainActivity.java
+++ Care2Reuse/app/src/main/java/com/example/christian/care2reuse/MainActivity.java
@@ -1,21 +1,37 @@
 package com.example.christian.care2reuse;
 
-import android.support.v7.app.ActionBarActivity;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-//import android.app.Activity;
-import android.content.Intent;
+import android.widget.EditText;
+import android.app.Activity;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+
+    EditText etResponse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        etResponse = (EditText) findViewById(R.id.editText);
+
+        new HttpAsyncTask().execute("https://dev.care2reuse.org/posts/?format=json");
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -61,6 +77,58 @@ public class MainActivity extends ActionBarActivity {
     {
         Intent intent = new Intent(MainActivity.this,Post.class);
         startActivity(intent);
+    }
+
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException{
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while((line = bufferedReader.readLine()) != null)
+            result += line;
+
+        inputStream.close();
+        return result;
+
+    }
+
+    public static String GET(String url){
+        InputStream inputStream = null;
+        String result = "";
+        try {
+
+            // create HttpClient
+            HttpClient httpclient = new DefaultHttpClient();
+
+            // make GET request to the given URL
+            HttpResponse httpResponse = httpclient.execute(new HttpGet(url));
+
+            // receive response as inputStream
+            inputStream = httpResponse.getEntity().getContent();
+
+            // convert inputstream to string
+            if(inputStream != null)
+                result = convertInputStreamToString(inputStream);
+            else
+                result = "Did not work!";
+
+        } catch (Exception e) {
+            Log.d("InputStream", e.getLocalizedMessage());
+        }
+
+        return result;
+    }
+
+    private class HttpAsyncTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            return GET(urls[0]);
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            etResponse.setText(result);
+        }
     }
 
 }
