dafc59b24264000f4d12bf636e76331da28d7b7e
christianstenderup <stenderup.christian@gmail.com>
2015-05-05 15:22:04 +0200

    removed some unused code

--- Care2Reuse/app/src/main/java/com/example/christian/care2reuse/loginFragment.java
+++ Care2Reuse/app/src/main/java/com/example/christian/care2reuse/loginFragment.java
@@ -24,13 +24,7 @@ public class loginFragment extends Fragment {
 
     private CallbackManager callbackManager;
     private LoginButton loginButton;
-    //private TextView skipLoginButton;
-    //private SkipLoginCallback skipLoginCallback;
-    /*
-    public interface SkipLoginCallback {
-        void onSkipLoginPressed();
-    }
-    */
+
     @Override
     public View onCreateView(
             LayoutInflater inflater,
@@ -38,47 +32,30 @@ public class loginFragment extends Fragment {
             Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.login, container, false);
-        //FacebookSdk.sdkInitialize(this.getApplicationContext());
         callbackManager = CallbackManager.Factory.create();
 
         loginButton = (LoginButton) view.findViewById(R.id.login_button);
         loginButton.setReadPermissions("user_friends");
-        // If using in a fragment
+
         loginButton.setFragment(this);
-        // Other app specific specialization
 
         // Callback registration
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                //App code
                 Toast.makeText(getActivity(), "Login successful", Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onCancel() {
-                // App code
                 Toast.makeText(getActivity(), "Login canceled", Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onError(FacebookException exception) {
-                // App code
                 Toast.makeText(getActivity(), "Login error", Toast.LENGTH_SHORT).show();
             }
         });
-        /*
-        skipLoginButton = (TextView) view.findViewById(R.id.skip_login_button);
-        skipLoginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (skipLoginCallback != null) {
-                    skipLoginCallback.onSkipLoginPressed();
-                }
-            }
-
-        });
-        */
         return view;
     }
 
@@ -87,9 +64,4 @@ public class loginFragment extends Fragment {
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
     }
-/*
-    public void setSkipLoginCallback(SkipLoginCallback callback) {
-        skipLoginCallback = callback;
-    }
-*/
 }
