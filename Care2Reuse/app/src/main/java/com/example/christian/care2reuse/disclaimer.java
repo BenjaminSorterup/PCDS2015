f4aa6ff1c1080b695ef75078ee475e8d97c1ad76
christianstenderup <stenderup.christian@gmail.com>
2015-05-19 20:36:22 +0200

    add sliding infopage

---
+++ Care2Reuse/app/src/main/java/com/example/christian/care2reuse/disclaimer.java
@@ -0,0 +1,32 @@
+package com.example.christian.care2reuse;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * disclaimer class is a fragment shows the disclaimer information
+ */
+public class disclaimer extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.disclaimer, container, false);
+
+        return v;
+    }
+
+    /**
+     *@return disclaimer fragment
+     */
+    public static disclaimer newInstance() {
+
+        disclaimer f = new disclaimer();
+
+        return f;
+    }
+}
---
+++ Care2Reuse/app/src/main/java/com/example/christian/care2reuse/innoaid.java
@@ -0,0 +1,29 @@
+package com.example.christian.care2reuse;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * innoaid class is a fragment shows information of the Innoaid
+ */
+public class innoaid extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.innoaid, container, false);
+
+        return v;
+    }
+
+    /**
+     *@return innoaid fragment
+     */
+    public static innoaid newInstance() {
+
+        innoaid f = new innoaid();
+
+        return f;
+    }
+}
