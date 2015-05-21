3c629912b3b30017627e8716abfd54c2cbafc7a6
BenjaminSorterup <benjamin@sorterup.com>
2015-05-21 20:35:05 +0200

    Working on distance seekbar

--- Care2Reuse/app/src/main/java/com/example/christian/care2reuse/MainActivity.java
+++ Care2Reuse/app/src/main/java/com/example/christian/care2reuse/MainActivity.java
@@ -7,15 +7,24 @@ import org.json.JSONObject;
 
 import android.app.ListActivity;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.FloatMath;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SeekBar;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
@@ -26,18 +35,29 @@ public class MainActivity extends ListActivity {
     //tags for receiving information from the JSON data
     private static final String TAG_ID = "id";
     private static final String TAG_CON = "content";
-    private static final String TAG_USER = "user"; //index at which user information is stored
+    private static final String TAG_USER = "user";
     private static final String TAG_FNAME = "first_name";
     private static final String TAG_LNAME = "last_name";
     private static final String TAG_DATE = "date_created";
+    private static final String TAG_LOC = "location";
 
     ArrayList<HashMap<String,String>> postList;
+
+    GPSTracker mGPS = new GPSTracker(this);
+    Double mLatitude;
+    Double mLongitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mLatitude = mGPS.getLocation().getLatitude();
+        mLongitude = mGPS.getLocation().getLongitude();
+
+
+        ImageButton postButton = (ImageButton) findViewById(R.id.button4);
+        postButton.bringToFront();
         postList = new ArrayList<HashMap<String, String>>();
 
         ListView lv = getListView();
@@ -50,6 +70,8 @@ public class MainActivity extends ListActivity {
                 String lastname = ((TextView) view.findViewById(R.id.lastname)).getText().toString();
                 String firstname = ((TextView) view.findViewById(R.id.firstname)).getText().toString();
                 String date = ((TextView) view.findViewById(R.id.date)).getText().toString();
+
+
 
                 Intent in = new Intent(MainActivity.this,SinglePostActivity.class);
                 in.putExtra(TAG_CON,content);
@@ -85,6 +107,27 @@ public class MainActivity extends ListActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private double distance(double lat1, double lon1, double lat2, double lon2, char unit) {
+        double theta = lon1 - lon2;
+        double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
+        dist = Math.acos(dist);
+        dist = rad2deg(dist);
+        dist = dist * 60 * 1.1515;
+        if (unit == 'K') {
+            dist = dist * 1.609344;
+        } else if (unit == 'N') {
+            dist = dist * 0.8684;
+        }
+        return (dist);
+    }
+    private double deg2rad(double deg) {
+        return (deg * Math.PI / 180.0);
+    }
+
+    private double rad2deg(double rad) {
+        return (rad * 180 / Math.PI);
     }
 
     public void toInfopage(View view)
@@ -136,7 +179,12 @@ public class MainActivity extends ListActivity {
                         JSONObject user = jObj.getJSONObject(TAG_USER);
                         String first_name = user.getString(TAG_FNAME);
                         String last_name = user.getString(TAG_LNAME);
-                        Log.e("TESTERINO",date);
+                        JSONArray location = jObj.getJSONObject(TAG_LOC).getJSONArray("coordinates");
+                        Double latitude = location.getDouble(0);
+                        Double longitude = location.getDouble(1);
+
+                        Double distance = distance(mLatitude,mLongitude,latitude,longitude,'K');
+                        Log.e("DISTANCE",""+distance);
 
                         //add to listview
                         HashMap<String,String> post = new HashMap<String,String>();
@@ -145,6 +193,9 @@ public class MainActivity extends ListActivity {
                         post.put(TAG_DATE,date);
                         post.put(TAG_FNAME,first_name);
                         post.put(TAG_LNAME,last_name);
+                        post.put("distance",distance.toString());
+
+
 
                         postList.add(post);
                     }
@@ -160,11 +211,45 @@ public class MainActivity extends ListActivity {
         protected void onPostExecute(Void result) {
             super.onPostExecute(result);
             //create list adapter from information gained from the JSON data
-            ListAdapter adapter = new SimpleAdapter(
+            final SimpleAdapter adapter = new SimpleAdapter(
                     MainActivity.this,postList,
-                    R.layout.list_v, new String[]{TAG_CON,TAG_DATE,TAG_FNAME,TAG_LNAME},
-                    new int[]{R.id.content,R.id.date,R.id.firstname,R.id.lastname});
+                    R.layout.list_v, new String[]{TAG_CON,TAG_DATE,TAG_FNAME,TAG_LNAME,"latitude"},
+                    new int[]{R.id.content,R.id.date,R.id.firstname,R.id.lastname,R.id.distance});
             setListAdapter(adapter);
+            EditText search = (EditText)findViewById(R.id.search);
+            SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar);
+            seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+                    Log.e("DISTANCE","touched the seekbar");
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+
+                }
+            });
+            search.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+
+                }
+
+                @Override
+                public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+                    adapter.getFilter().filter(charSequence);
+                }
+
+                @Override
+                public void afterTextChanged(Editable editable) {
+
+                }
+            });
         }
     }
 }
